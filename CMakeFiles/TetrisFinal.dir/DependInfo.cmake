4b2bc269d876a57ef4a03396a3805f85ac4b72a5
celestet <houseoftan4@gmail.com>
2021-03-05 12:41:04 -0800

    commit inside c folders

---
+++ CMakeFiles/TetrisFinal.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/celeste/Desktop/folders/PCC/sophmore/fall/cs3A-smith/exam projects/TetrisFinal/Blocks.cpp" "/Users/celeste/Desktop/folders/PCC/sophmore/fall/cs3A-smith/exam projects/TetrisFinal/cmake-build-debug/CMakeFiles/TetrisFinal.dir/Blocks.cpp.o"
+  "/Users/celeste/Desktop/folders/PCC/sophmore/fall/cs3A-smith/exam projects/TetrisFinal/Game.cpp" "/Users/celeste/Desktop/folders/PCC/sophmore/fall/cs3A-smith/exam projects/TetrisFinal/cmake-build-debug/CMakeFiles/TetrisFinal.dir/Game.cpp.o"
+  "/Users/celeste/Desktop/folders/PCC/sophmore/fall/cs3A-smith/exam projects/TetrisFinal/NextPiece.cpp" "/Users/celeste/Desktop/folders/PCC/sophmore/fall/cs3A-smith/exam projects/TetrisFinal/cmake-build-debug/CMakeFiles/TetrisFinal.dir/NextPiece.cpp.o"
+  "/Users/celeste/Desktop/folders/PCC/sophmore/fall/cs3A-smith/exam projects/TetrisFinal/main.cpp" "/Users/celeste/Desktop/folders/PCC/sophmore/fall/cs3A-smith/exam projects/TetrisFinal/cmake-build-debug/CMakeFiles/TetrisFinal.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/SFML.framework"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/TetrisFinal.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TetrisFinal.dir/Blocks.cpp.o"
+  "CMakeFiles/TetrisFinal.dir/Game.cpp.o"
+  "CMakeFiles/TetrisFinal.dir/NextPiece.cpp.o"
+  "CMakeFiles/TetrisFinal.dir/main.cpp.o"
+  "TetrisFinal"
+  "TetrisFinal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TetrisFinal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
